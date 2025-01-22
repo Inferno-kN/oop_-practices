6319b786b5471c0f15dfb6653f3e63adf0916437
Inferno-kN <aldosovich@bk.ru>
2025-01-22 22:07:41 +0300

    Update main_2.py

--- hw_2/main_2.py
+++ hw_2/main_2.py
@@ -1,4 +1,4 @@
-#Задача 1
+# Задача 1
 
 class Patient:
     def __init__(self, fio: str, age: int, disease: str):
@@ -12,7 +12,8 @@ class Patient:
     def info(self):
         print(f"ФИО: {self.fio}, Возраст: {self.age}, Текущее заболевание: {self.disease}")
 
-#Задача 2
+
+# Задача 2
 
 class TouristSpot:
     def __init__(self, place: str, country: str, sight: str):
@@ -24,14 +25,14 @@ class TouristSpot:
         print(f"{name}, Вы посетили нас!")
 
     def info(self):
-        print(f"Место: {self.place}, Страна: {self.country}, Достопримечательность: {self.dostopr}")
+        print(f"Место: {self.place}, Страна: {self.country}, Достопримечательность: {self.sight}")
 
-#Задача 3
+
+# Задача 3
 
 class ModelWindow:
-
-    width_screen = 1960 #ширина экрана, а это горизонталь gorizont
-    height_screen = 1080 #высота экрана, то есть вертикаль vertikal
+    width_screen = 1960  # ширина экрана, а это горизонталь gorizont
+    height_screen = 1080  # высота экрана, то есть вертикаль vertikal
 
     def __init__(self, title: str, koord: int, vertikal: int, gorizont: int, color: str, visible: bool, frame: bool):
         self.title = title
@@ -43,8 +44,8 @@ class ModelWindow:
         self.frame = frame
 
     def movement_vertikal(self, delta) -> bool:
-       new_y = self.koord_y + delta
-       if 0 <= new_y <= ModelWindow.height_screen - self.height:
+        new_y = self.koord_y + delta
+        if 0 <= new_y <= ModelWindow.height_screen - self.height:
             self.koord_y = new_y
             print(f"Окно перемещено по вертикали на {delta}, Текущая координата Y: {self.koord_y}")
             return True
@@ -62,14 +63,14 @@ class ModelWindow:
             print(f"Перемещение по горизонтали на {delta} невозможно. Окно выходит за границы экрана.")
             return False
 
-
     def change_height(self, new_height: int) -> bool:
         if 0 <= new_height <= ModelWindow.height_screen:
             self.height = new_height
             print(f"Высота окна изменена на {new_height}.")
             return True
         else:
-            print(f"Недопустимая высота: {new_height}. Высота должна быть в пределах от 0 до {ModelWindow.height_screen}.")
+            print(
+                f"Недопустимая высота: {new_height}. Высота должна быть в пределах от 0 до {ModelWindow.height_screen}.")
             return False
 
     def change_width(self, new_width: int) -> bool:
@@ -78,7 +79,8 @@ class ModelWindow:
             print(f"Ширина окна изменена на {new_width}.")
             return True
         else:
-            print(f"Недопустимая ширина: {new_width}. Ширина должна быть в пределах от 0 до {ModelWindow.width_screen}.")
+            print(
+                f"Недопустимая ширина: {new_width}. Ширина должна быть в пределах от 0 до {ModelWindow.width_screen}.")
             return False
 
     def update_color(self, new_color: str) -> bool:
@@ -104,7 +106,8 @@ class ModelWindow:
     def __str__(self):
         return f"Заголовок окна: {self.title}, Координаты левого верхнего угла: {self.koord},\nРазмер по горизонтали, Размер по вертикали:{self.gorizont}, {self.vertikal}, Цвет окна: {self.color}, Cостояние видимое/невидимое: {self.visible}, состояние с рамкой/без рамки: {self.frame}"
 
-#Задача 4
+
+# Задача 4
 
 class ArrayUtils:
 
@@ -137,7 +140,8 @@ class ArrayUtils:
             min1 = arr[0]
         return min1
 
-#Задача 5
+
+# Задача 5
 
 class Vector:
     def __init__(self, x: float, y: float, z: float):
@@ -148,32 +152,33 @@ class Vector:
     def __add__(self, other):
         if isinstance(other, Vector):
             return Vector(self.x + other.x, self.y + other.y, self.z + other.z)
-            
+
     def __sub__(self, other):
         if isinstance(other, Vector):
             return Vector(self.x - other.x, self.y - other.y, self.z - other.z)
-            
+
     def __mul__(self, other):
         if isinstance(other, Vector):
-            return self.x * other.x + self.y * other.y + self.z * other.z # Скалярное произведение
+            return self.x * other.x + self.y * other.y + self.z * other.z  # Скалярное произведение
 
-     def cross(self, other): # Векторное произведение
+    def cross(self, other):  # Векторное произведение
         if isinstance(other, Vector):
             new_x = self.y * other.z - self.z * other.y
             new_y = self.z * other.x - self.x * other.z
             new_z = self.x * other.y - self.y * other.x
-            return Vector(x_component, y_component, z_component)
+            return Vector(new_x, new_y, new_z)
 
     def length(self):
-        return math.sqrt(self.x**2 + self.y**2 + self.z**2)
+        return math.sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
 
     def __str__(self):
         return f"{self.x}, {self.y}, {self.z}"
 
-#Задача 6
+
+# Задача 6
 
 class Fraction:
-    def __init__(self, numerator: int, denominator: int): # numerator - числитель, denominator - знаменатель
+    def __init__(self, numerator: int, denominator: int):  # numerator - числитель, denominator - знаменатель
         self.numerator = numerator
         self.denominator = denominator
 
@@ -185,7 +190,6 @@ class Fraction:
         else:
             raise ValueError("Знаменатель не может быть равен нулю")
 
-
     def __sub__(self, other):
         if self.denominator != 0:
             new_numerator = self.numerator - other.numerator
@@ -193,7 +197,6 @@ class Fraction:
             return new_numerator, new_denominator
         else:
             raise ValueError("Знаменатель не может быть равен нулю")
-
 
     def __mul__(self, other):
         if self.denominator != 0:
@@ -210,27 +213,28 @@ class Fraction:
             return self.numerator / self.denominator
 
 
-#Задача 7
+# Задача 7
 import math
+
 
 class GeometryUtils:
 
-    @staticmethod #1
+    @staticmethod  # 1
     def calculating_circle(radius: float) -> float:
-        S = math.pi * radius**2 # рассчитываем площадь круга
+        S = math.pi * radius ** 2  # рассчитываем площадь круга
         return S
 
-    @staticmethod #2
+    @staticmethod  # 2
     def perimeter_circle(radius: float) -> float:
         P = 2 * math.pi * radius
         return P
 
-    @staticmethod #3
+    @staticmethod  # 3
     def rectangle_area(length: float, width: float) -> float:
-        S = length * width #площадь прямоугольника
+        S = length * width  # площадь прямоугольника
         return S
 
-    @staticmethod #4
+    @staticmethod  # 4
     def perimeter_area(length: float, width: float) -> float:
         P = 2 * (length + width)
         return P
@@ -242,11 +246,11 @@ class GeometryUtils:
         return Geron
 
 
-#Задача 8
+# Задача 8
 
 class Time:
     def __init__(self, hours: int, minutes: int, seconds: int):
-        self.hours = hours + (minutes // 60) # если задано минут > 60, то присваиваем целое
+        self.hours = hours + (minutes // 60)  # если задано минут > 60, то присваиваем целое
         self.minutes = (minutes % 60) + (seconds // 60)
         self.seconds = seconds % 60
 
@@ -270,4 +274,3 @@ class Time:
 
     def __str__(self):
         return f"{(self.hours % 100) // 10}{(self.hours // 1) % 10}:{(self.minutes % 100) // 10}{self.minutes % 10}:{(self.seconds % 100) // 10}{self.seconds % 10}"
-
